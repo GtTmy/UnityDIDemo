291541039204767605ef9ac70acc66596fa88b35
GtTmy <tomoya.gotoh.wind@gmail.com>
2018-11-08 22:36:56 +0900

    added comments

--- ModuleA/ModuleAModule.cs
+++ ModuleA/ModuleAModule.cs
@@ -12,13 +12,18 @@ namespace ModuleA
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            // 画面遷移を行う
+            // 今回の本題とは関係ないので説明略
             var regionManager = containerProvider.Resolve<IRegionManager>();
             regionManager.RequestNavigate("ContentRegion", nameof(ViewA));
         }
 
+        // ここでコンテナへの登録作業を行う
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            // Unityのコンテナに直接アクセスできる
+            // ContainerRegistryが抽象化されたコンテナへの登録用Interface
+
+            // GetContainerでUnityのコンテナに直接アクセスできる
             // NugetにてUnity、Prism.Unityを導入する必要あり
             var container = containerRegistry.GetContainer();
 
